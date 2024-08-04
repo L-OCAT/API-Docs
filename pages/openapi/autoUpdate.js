745ab2cc3039f91f98b5cd4a74bb6dad9bc952cf
SeongHo5 <newfuring1123@gmail.com>
2024-08-04 20:05:14 +0900

    Update github-pages workflow
    - OpenAPI YAML도 포함되도록 수정

--- pages/openapi/autoUpdate.js
+++ pages/openapi/autoUpdate.js
@@ -6,7 +6,7 @@ const STANDARD_CHARSET = 'UTF-8';
 
 const baseDirectory = process.env.GITHUB_WORKSPACE || path.resolve(__dirname, '../..');
 const apiYamlDirectory = path.join(baseDirectory, 'openapi');
-const swaggerPath = path.join(baseDirectory, 'pages/swagger-initializer.js');
+const swaggerPath = path.join(baseDirectory, '../swagger-initializer.js');
 
 function readFile(filePath, encoding = STANDARD_CHARSET) {
     try {
--- pages/swagger-initializer.js
+++ pages/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [{
-      url: "../openapi/schemas.yaml",
+      url: "./openapi/schemas.yaml",
       name: "Common Schema"
     }
     ],
