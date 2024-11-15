3d5c83ceb2ce49e91db26c20c2f44781a40ef1f3
SeongHo5 <newfuring1123@gmail.com>
2024-11-15 21:55:57 +0900

    Remove duplicate docs

--- pages/swagger-initializer.js
+++ pages/swagger-initializer.js
@@ -1,21 +1,31 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
+window.onload = function () {
+    //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUIBundle({
-    urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}, {url: "./openapi/v1_user.yaml", name: "User API"}, {url: "./openapi/v1_auth.yaml", name: "Auth API"}, {url: "./openapi/v1_found.yaml", name: "Found Item API"}, {url: "./openapi/v1_lost.yaml", name: "Lost Item API"}, {url: "./openapi/v1_common.yaml", name: "Common API"}, {url: "./openapi/v1_terms.yaml", name: "Terms API"}],
-    dom_id: '#swagger-ui',
-    deepLinking: true,
-    docExpansion: 'full',
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
-  });
+    // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+    window.ui = SwaggerUIBundle({
+        urls: [{url: "./openapi/schemas.yaml", name: "Common Schema"}, {
+            url: "./openapi/v1_user.yaml",
+            name: "User API"
+        }, {url: "./openapi/v1_auth.yaml", name: "Auth API"}, {
+            url: "./openapi/v1_found.yaml",
+            name: "Found Item API"
+        }, {url: "./openapi/v1_lost.yaml", name: "Lost Item API"}, {
+            url: "./openapi/v1_common.yaml",
+            name: "Common API"
+        }
+        ],
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        docExpansion: 'full',
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout"
+    });
 
-  //</editor-fold>
+    //</editor-fold>
 };
